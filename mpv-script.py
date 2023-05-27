83d88baa116e803603bb5d4f1f5c10227338fbd8
Brett Barker <brett.barker@brbtechsolutions.com>
2023-05-26 21:59:59 -0500

    Adding MPV Stream Script

---
+++ mpv-script.py
@@ -0,0 +1,38 @@
+import mpv
+import time
+
+# Define the URL of the stream
+stream_url = ""
+print("Stream URL: " + stream_url)
+
+# Create an MPV player instance
+player = mpv.MPV()
+
+# Uncomment the following line to enable fullscreen
+# player.fullscreen = True
+
+# Load the stream URL
+player.play(stream_url)
+
+# Set the initial playback time
+lastPlaybackTime = 0
+
+# Set the maximum time for the stream to be frozen (in seconds)
+max_frozen_time = 10
+
+# Allow stream 10 seconds to start before checking
+time.sleep(10)
+
+# Loop indefinitely
+while True:
+    print("Playback time: " + str(player.playback_time))
+    # Wait for a short time
+    time.sleep(0.5)
+
+    if lastPlaybackTime == player.playback_time:
+        print("Stream is frozen")
+        # restart stream
+        player.play(stream_url)
+        time.sleep(10)
+
+    lastPlaybackTime = player.playback_time
